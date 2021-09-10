1ffdbee73fd101464d816845c986fe7d9a2db70e
maayazed <mayahzebrasky@gmail.com>
2021-09-09 21:51:23 -0500

    added nav display. and about display, tweaked build process, and app display with imports

---
+++ src/components/About.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import '../styles/About.css';
+import Background from '../img/intro.png';
+
+const styles = {
+  overlayStyle: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignContent: 'normal',
+    alignItems: 'center',
+    backgroundColor: '#333333be',
+    color: '#ffffff'
+  },
+  overlayContentStyle: {
+    position: 'static',
+    display: 'flex',
+    left: 'auto',
+    marginTop: '1px',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'flex - start',
+    textAlign: 'center',
+  },
+};
+
+function About() {
+  return (
+    <div style={styles.overlayStyle} className='section'>
+      <div style={styles.overlayContentStyle}>
+        <h1>About Section</h1>
+        <p><a href='https://maayazed.github.io/prework-about-me/'><img src={Background} alt='About section img' /></a></p>
+      </div>
+    </div>
+  );
+}
+
+export default About;
