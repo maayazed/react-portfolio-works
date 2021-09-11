34827e6514af4b6556abd7a46c85c214a9b96894
maayazed <mayahzebrasky@gmail.com>
2021-09-11 17:02:11 -0500

    finished body display with links, next resume download

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -7,8 +7,13 @@ function Nav() {
       <nav>
         <ul>
           <li><a href="#works">Works</a></li>
-          <li><a href="https://github.com/maayazed">Github</a></li>
-          <li><a href="https://www.linkedin.com/in/maaya-zed-b61b35219?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BuGoSU%2B1XSUWytp7DH91lJQ%3D%3D">Linkedin</a></li>
+          <li><a href="https://github.com/maayazed" target="_blank" rel="noopener noreferrer">
+            Github
+          </a></li>
+          <li><a href="https://www.linkedin.com/in/maaya-zed-b61b35219?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BuGoSU%2B1XSUWytp7DH91lJQ%3D%3D"
+            target="_blank" rel="noopener noreferrer">
+            Linkedin
+          </a></li>
           <li>zerreod@outlook.com</li>
         </ul>
       </nav>
