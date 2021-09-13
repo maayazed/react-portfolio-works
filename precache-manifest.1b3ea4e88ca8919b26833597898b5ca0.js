1bc511e40b788d2ab0f30cc962ffdc376d8853af
maayazed <mayahzebrasky@gmail.com>
2021-09-12 22:36:19 -0500

    Updates

---
+++ precache-manifest.1b3ea4e88ca8919b26833597898b5ca0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "afe0e61f65c13c7ee311",
+    "url": "/static/css/main.c4dc33e1.chunk.css"
+  },
+  {
+    "revision": "afe0e61f65c13c7ee311",
+    "url": "/static/js/main.b7d84a6a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "e822b6d1f2c1e181e8bd",
+    "url": "/static/js/2.8d510b5f.chunk.js"
+  },
+  {
+    "revision": "93558e0185ba0fec595e2501672ce114",
+    "url": "/static/media/Resume-MAAYAZED.93558e01.pdf"
+  },
+  {
+    "revision": "57fc89be52172aabcba21dfe1a1ba52a",
+    "url": "/static/media/intro.57fc89be.png"
+  },
+  {
+    "revision": "83dc7a2ad0f83956a1d16b0b158c6239",
+    "url": "/index.html"
+  }
+];
